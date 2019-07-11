911b3427ba4e6c85ac07ca2aa0d051d51eb9a5c2
Raghavendra Desiraju <RDesiraju@houston.personix.local>
2019-07-11 12:22:49 -0500

    fixed typo error

--- DatingApp-SPAPractice/src/app/app.module.ts
+++ DatingApp-SPAPractice/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
       ValueComponent
    ],
    imports: [
-      BrowserModule,s
+      BrowserModule,
       HttpClientModule
    ],
    providers: [],
