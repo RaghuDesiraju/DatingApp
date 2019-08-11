008de70ee29c56bd3171590f0aa4e5554cb789ef
Raghavendra Desiraju <RDesiraju@houston.personix.local>
2019-08-10 19:00:28 -0500

    Chapter 6 Add 3rd party components and styles

---
+++ DatingApp-SPA/src/app/_services/alertify.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+declare let alertify: any; // we need to declare before using this
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertifyService {
+
+constructor() { }
+
+// e means click event on ok button
+confirm(message: string, okCallback: () => any) {
+  alertify.confirm(message, function(e) {
+    if (e) {
+      okCallback();
+    } else {}
+  });
+}
+
+success(message: string) {
+  alertify.success(message);
+}
+
+error(message: string) {
+  alertify.error(message);
+}
+
+warning(message: string) {
+  alertify.warning(message);
+}
+
+message(message: string) {
+  alertify.message(message);
+}
+
+}
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,7 +9,7 @@ import { AuthService } from '../_services/auth.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -16,22 +17,23 @@ export class NavComponent implements OnInit {
   login() {
     console.log(this.model);
     this.authService.login(this.model).subscribe(next => {
-      console.log('logged in successfully to webapi');
+    this.alertify.success('Logged in successfully');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
+    //const token = localStorage.getItem('token');
+    //return !!token;
     // return false if token is empty
 
   }
 
   logOut() {
     localStorage.removeItem('token');
-   console.log('logged out');
+      this.alertify.success('Logged out');
     // return false if token is empty
   }
 
